8d57b26f292d17c9a1bb785cd7a9c0ddc819502b
Benedikt Hitz <bhitz@gmx.net>
2025-01-21 15:40:22 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Onto Test
+
+This repository is a test to try wheter the content of a .ttl ontology can be displayed properly within a [ReSpec](https://respec.org) document.
---
+++ config.js
@@ -0,0 +1,23 @@
+var respecConfig = {
+    specStatus: "unofficial",
+    shortName: 'onto',
+    edDraftURI: "https://bequrios.github.io/onto",
+    format: 'markdown', 
+    latestVersion: null,
+    maxTocLevel: 4,
+    preProcess: [],
+    postProcess: [],
+    github: {
+      repoURL: "https://github.com/bequrios/onto",
+      branch: "main", // alternative branch
+    },
+    editors: [
+    {
+      name: "Benedikt Hitz-Gamper",
+      company: "Bern University of Applied Sciences (BFH)",
+      companyURL: "https://www.bfh.ch",
+      url: "mailto:benedikt.hitz@bfh.ch",
+    }]
+  };
+
+module.exports = respecConfig
